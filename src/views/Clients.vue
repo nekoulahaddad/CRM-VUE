980a44a5790fd5ed4c671eb8a9c0d89fcd144e92
Halid <denievhalid88@mail.ru>
2022-01-01 00:22:11 +0300

    commit

--- src/views/Clients.vue
+++ src/views/Clients.vue
@@ -6,7 +6,36 @@
       </div>
       <h1 class="page__title">Клиенты</h1>
     </div>
-    <div class="page__body"></div>
+    <div class="page__body d-flex">
+      <div class="filter">
+        <div class="filter__inner">
+          <div class="filter__header">
+            <div class="filter__title">Фильтр</div>
+          </div>
+          <div class="filter__body">
+            <div class="filter__group group">
+              <div class="group__title">Регионы:</div>
+              <div class="group__content">
+                <select class="form-select">
+                  <option value="Все задачи">Все регионы</option>
+                </select>
+              </div>
+            </div>
+            <div class="filter__group group">
+              <div class="group__title">Сортировка:</div>
+              <div class="group__content">
+                <select class="form-select">
+                  <option value="Все задачи">Интернет-магазин</option>
+                </select>
+              </div>
+            </div>
+            <div class="filter__actions">
+              <button class="btn btn--red filter__btn">Очистить</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
