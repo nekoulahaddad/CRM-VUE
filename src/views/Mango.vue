2cb1e46bcafa5013cb1c81793005fd7b36bd6882
Halid <denievhalid88@mail.ru>
2022-01-01 00:27:14 +0300

    commit

--- src/views/Mango.vue
+++ src/views/Mango.vue
@@ -6,7 +6,28 @@
       </div>
       <h1 class="page__title">Mango</h1>
     </div>
-    <div class="page__body"></div>
+    <div class="page__body d-flex">
+      <div class="filter">
+        <div class="filter__inner">
+          <div class="filter__header">
+            <div class="filter__title">Фильтр</div>
+          </div>
+          <div class="filter__body">
+            <div class="filter__group group">
+              <div class="group__title">Дата:</div>
+              <div class="group__content">
+                <select class="form-select">
+                  <option value="Все задачи">Выберите дату</option>
+                </select>
+              </div>
+            </div>
+            <div class="filter__actions">
+              <button class="btn btn--red filter__btn">Очистить</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
