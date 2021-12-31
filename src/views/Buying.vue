de1340bd1d86bab5864763936882dded87b4e8c7
Halid <denievhalid88@mail.ru>
2022-01-01 01:01:39 +0300

    commit

--- src/views/Buying.vue
+++ src/views/Buying.vue
@@ -1,5 +1,70 @@
 <template>
-  <div>Buying.vue</div>
+  <div class="page">
+    <div class="page__header">
+      <div class="page__icon">
+        <img src="/icons/buying_title.svg" alt="" />
+      </div>
+      <h1 class="page__title">Закупка</h1>
+    </div>
+    <div class="page__body d-flex">
+      <div class="filter">
+        <div class="filter__inner">
+          <div class="filter__header">
+            <div class="filter__title">Фильтр</div>
+          </div>
+          <div class="filter__body">
+            <div class="filter__group group">
+              <div class="group__title">Период:</div>
+              <div class="group__content">
+                <select class="form-select">
+                  <option value="Все задачи">Все периоды</option>
+                </select>
+              </div>
+            </div>
+            <div class="filter__group group">
+              <div class="group__title">Регионы:</div>
+              <div class="group__content">
+                <select class="form-select">
+                  <option value="Все задачи">Все регионы</option>
+                </select>
+              </div>
+            </div>
+            <div class="filter__group group">
+              <div class="group__title">Автор:</div>
+              <div class="group__content">
+                <input
+                  class="form-control"
+                  type="text"
+                  placeholder="Введите автора задачи..."
+                />
+              </div>
+            </div>
+            <div class="filter__group group">
+              <div class="group__title">Исполнитель:</div>
+              <div class="group__content">
+                <input
+                  class="form-control"
+                  type="text"
+                  placeholder="Введите исполнителя задачи..."
+                />
+              </div>
+            </div>
+            <div class="filter__group group">
+              <div class="group__title">Статусы:</div>
+              <div class="group__content">
+                <select class="form-select">
+                  <option value="Все задачи">Все статусы:</option>
+                </select>
+              </div>
+            </div>
+            <div class="filter__actions">
+              <button class="btn btn--red filter__btn">Очистить</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
