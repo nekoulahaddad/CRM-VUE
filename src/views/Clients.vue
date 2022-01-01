94977f3326bbce6bcfa0770592b2a8e6c3bd12a6
Halid <denievhalid88@mail.ru>
2022-01-01 03:38:23 +0300

    commit

--- src/views/Clients.vue
+++ src/views/Clients.vue
@@ -35,10 +35,91 @@
           </div>
         </div>
       </div>
+      <div class="flex-1">
+        <table class="table">
+          <thead>
+            <tr>
+              <td colspan="8">Клиенты</td>
+            </tr>
+            <tr>
+              <td>№:</td>
+              <td>Клиент</td>
+              <td>Почта:</td>
+              <td>Телефон:</td>
+              <td>Регион:</td>
+              <td>Дата:</td>
+              <td>Сумма:</td>
+              <td>Клубная карта:</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Аракелов С.В.</td>
+              <td>info@gmail.com</td>
+              <td>+7 415 000 00 00</td>
+              <td>Москва и М.О</td>
+              <td>13.12.2021</td>
+              <td>7409.37 BYN</td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Tasks from "./Tasks";
+export default {
+  components: { Tasks },
+  data() {
+    return {
+      columns: [
+        { label: "id", field: "id" },
+        {
+          label: "Username",
+          field: "user.username",
+          headerClass: "class-in-header second-class",
+        },
+        { label: "First Name", field: "user.firstName" },
+        { label: "Last Name", field: "user.lastName" },
+        { label: "Email", field: "user.email" },
+        {
+          label: "Address",
+          representedAs: ({ address, city, state }) =>
+            `${address}<br />${city}, ${state}`,
+          interpolate: true,
+        },
+      ],
+      rows: [
+        {
+          id: 1,
+          user: {
+            username: "dprice0",
+            firstName: "Daniel",
+            lastName: "Price",
+            email: "dprice0@blogs.com",
+          },
+          address: "3 Toban Park",
+          city: "Pocatello",
+          state: "Idaho",
+        },
+        {
+          id: 2,
+          user: {
+            username: "dprice0",
+            firstName: "Daniel",
+            lastName: "Price",
+            email: "dprice0@blogs.com",
+          },
+          address: "3 Toban Park",
+          city: "Pocatello",
+          state: "Idaho",
+        },
+      ],
+    };
+  },
+};
 </script>
