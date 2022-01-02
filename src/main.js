0bdc6ed51eb7e8b5f53dd419ffbc6d89f2ef5d31
Halid <denievhalid88@mail.ru>
2022-01-02 15:44:31 +0300

    commit

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import store from "./store";
 import router from "./router";
 import VueNumberFormat from "vue-number-format";
 import Paginate from "vuejs-paginate";
+import Toast from "vue-toastification";
 import VueMoment from "vue-moment";
 import i18n from "./i18n";
 import "moment/locale/ru";
+import "vue-toastification/dist/index.css";
 import "./styles/index.scss";
 
 Vue.config.productionTip = false;
@@ -17,6 +19,13 @@ Vue.use(VueNumberFormat, {
   thounsand: ".",
   precision: 0,
   isInteger: true,
+});
+Vue.use(Toast, {
+  transition: "Vue-Toastification__bounce",
+  maxToasts: 6,
+  newestOnTop: true,
+  position: "bottom-left",
+  timeout: 3500,
 });
 Vue.component("paginate", Paginate);
 
