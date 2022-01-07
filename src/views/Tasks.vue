f28f4f23e65c6b58513050022894b3c292c4fb2e
Halid <denievhalid88@mail.ru>
2022-01-07 23:53:14 +0300

    commit

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -124,8 +124,70 @@
                 <!-- Блок с детальной информацией о задаче -->
                 <div class="list__info list-info">
                   <div class="list-info__header">
-                    <div class="list-info__title text--blue">
-                      {{ $t("pages.tasks.info") }}
+                    <div class="list-info__group group">
+                      <div class="group__title text--blue">
+                        {{ $t("pages.tasks.taskInfo") }}
+                      </div>
+                      <div class="group__content">
+                        <div class="group__item text--bold-700">
+                          {{ $t("pages.tasks.taskName") }}
+                        </div>
+                        <div class="group__value">{{ item.title }}</div>
+                      </div>
+                      <div class="group__content">
+                        <div class="group__item text--bold-700">
+                          {{ $t("pages.tasks.taskAuthor") }}
+                        </div>
+                        <div class="group__value">123</div>
+                      </div>
+                      <div class="group__content">
+                        <div class="group__item text--bold-700">
+                          {{ $t("pages.tasks.taskDescription") }}
+                        </div>
+                        <div class="group__value">123</div>
+                      </div>
+                      <div class="group__content">
+                        <div class="group__item text--bold-700">
+                          {{ $t("pages.tasks.taskDocs") }}
+                        </div>
+                        <div class="group__value">123</div>
+                      </div>
+                      <div class="group__content">
+                        <div class="group__item text--bold-700">
+                          {{ $t("pages.tasks.taskRegion") }}
+                        </div>
+                        <div class="group__value">123</div>
+                      </div>
+                      <div class="group__content">
+                        <div class="group__item text--bold-700">
+                          {{ $t("pages.tasks.taskStatus") }}
+                        </div>
+                        <div class="group__value">123</div>
+                      </div>
+                      <div class="group__content">
+                        <div class="group__item text--bold-700">
+                          {{ $t("pages.tasks.taskMark") }}
+                        </div>
+                        <div class="group__value">123</div>
+                      </div>
+                      <div class="group__content">
+                        <div class="group__item text--bold-700">
+                          {{ $t("pages.tasks.taskCreated") }}
+                        </div>
+                        <div class="group__value">123</div>
+                      </div>
+                      <div class="group__content">
+                        <div class="group__item text--bold-700">
+                          {{ $t("pages.tasks.taskDeadline") }}
+                        </div>
+                        <div class="group__value">123</div>
+                      </div>
+                      <div class="group__content">
+                        <div class="group__item text--bold-700">
+                          {{ $t("pages.tasks.taskComment") }}
+                        </div>
+                        <div class="group__value">123</div>
+                      </div>
                     </div>
                   </div>
                 </div>
