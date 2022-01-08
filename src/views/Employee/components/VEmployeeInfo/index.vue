b587237a90ac31a17f7669b4790cf557a0ede4bd
Halid <denievhalid88@mail.ru>
2022-01-08 14:19:42 +0300

    commit

--- src/views/Employee/components/VEmployeeInfo/index.vue
+++ src/views/Employee/components/VEmployeeInfo/index.vue
@@ -3,6 +3,30 @@
     <div class="group__title text--blue">
       {{ $t("pages.employee.employeeMainInfo") }}
     </div>
+    <div class="list-info__group group">
+      <div class="group__content">
+        <div class="group__item text--bold-700">
+          {{ $t("pages.employee.employeeSurName") }}
+        </div>
+        <div class="group__value">{{ employee.surname }}</div>
+      </div>
+    </div>
+    <div class="list-info__group group">
+      <div class="group__content">
+        <div class="group__item text--bold-700">
+          {{ $t("pages.employee.employeeName") }}
+        </div>
+        <div class="group__value">{{ employee.name }}</div>
+      </div>
+    </div>
+    <div class="list-info__group group">
+      <div class="group__content">
+        <div class="group__item text--bold-700">
+          {{ $t("pages.employee.employeeLastName") }}
+        </div>
+        <div class="group__value">{{ employee.lastname }}</div>
+      </div>
+    </div>
     <div class="group__title text--blue">
       {{ $t("pages.employee.employeeAddInfo") }}
     </div>
@@ -13,5 +37,12 @@
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    employee: {
+      type: Object,
+      required: true,
+    },
+  },
+};
 </script>
