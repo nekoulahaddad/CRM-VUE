4e72ca9786ab9258b9fb9249f89155ebda4d1069
Halid <denievhalid88@mail.ru>
2022-01-08 05:23:35 +0300

    commit

--- src/views/Tasks/components/VEdit/index.vue
+++ src/views/Tasks/components/VEdit/index.vue
@@ -2,7 +2,7 @@
   <div class="list__edit edit">
     <div class="edit__inner">
       <div class="edit__title text--blue">{{ $t("pages.tasks.taskEdit") }}</div>
-      <div class="filter__group group">
+      <div class="group">
         <div class="group__title">Наименоване задачи:</div>
         <div class="group__content">
           <input
@@ -13,7 +13,7 @@
           />
         </div>
       </div>
-      <div class="filter__group group">
+      <div class="group">
         <div class="group__title">Описание задачи:</div>
         <div class="group__content">
           <textarea
@@ -24,16 +24,89 @@
           />
         </div>
       </div>
+      <div class="group">
+        <div class="group__title">Комментарий:</div>
+        <div class="group__content">
+          <textarea
+            type="text"
+            class="form-textarea"
+            v-model="task.initiator_comment"
+            placeholder="Введите комментарий к задаче..."
+          />
+        </div>
+      </div>
+      <div class="group">
+        <div class="group__title">Документы</div>
+        <div class="group__content">
+          <div v-if="task.documents.length" class="list__documents documents">
+            <div
+              v-for="(photo, index) in task.documents"
+              class="documents__item"
+              @click.prevent="downloadItem(serverAddr + `${photo}`, photo)"
+            >
+              {{
+                photo.name
+                  ? photo.name.length > 30
+                    ? photo.name.slice(0, -10) +
+                      " ... ." +
+                      photo.type.split("/")[1]
+                    : photo.name
+                  : `Документ ${index + 1}`
+              }}
+            </div>
+          </div>
+          <div v-else>Нет документов</div>
+        </div>
+      </div>
+      <div class="group">
+        <div class="group__footer">
+          <v-button red>Отправить</v-button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import VButton from "@/components/VButton";
+import axios from "@/api/axios";
+
 export default {
   props: {
     task: {
       type: Object,
       required: true,
+    },
+  },
+  data: () => {
+    return {
+      date: new Date(),
+      title: "",
+      description: "",
+      serverAddr: process.env.VUE_APP_DEVELOP_URL,
+      initiator_comment: "",
+      documents: [],
+    };
+  },
+  components: { VButton },
+  methods: {
+    downloadItem(url, filename) {
+      axios
+        .get(url, { responseType: "blob" })
+        .then((response) => {
+          const link = document.createElement("a");
+          const blob = new Blob([response.data]);
+          let urll = window.URL.createObjectURL(blob);
+          link.href = urll;
+          link.download = filename;
+          link.click();
+          setTimeout(() => {
+            window.URL.revokeObjectURL(urll);
+            document.body.removeChild(link);
+          }, 0);
+          URL.revokeObjectURL(link.href);
+        })
+        .catch(console.error);
     },
   },
 };
@@ -59,6 +132,18 @@ export default {
     &::placeholder {
       font-weight: 500;
     }
+  }
+}
+</style>
+
+<style lang="scss">
+.list__edit {
+  .form-textarea {
+    font-weight: 500;
+  }
+  button {
+    width: 230px;
+    height: 37px;
   }
 }
 </style>
