d34440a40dfcd80c6bd6125f6f79876adc114104
Halid <denievhalid88@mail.ru>
2022-01-09 06:13:15 +0300

    commit

--- src/views/Purchase/components/VInfo/index.vue
+++ src/views/Purchase/components/VInfo/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>VInfo</div>
+  <div class="list__info list-info">
+    <div class="group__title text--blue">
+      {{ $t("pages.buying.info") }}
+    </div>
+  </div>
 </template>
 
 <script>
