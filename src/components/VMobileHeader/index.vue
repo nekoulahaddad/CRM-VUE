5b6381b71ec4397bf655081eebe1e2f802b79fca
Halid <denievhalid88@mail.ru>
2022-01-09 04:20:10 +0300

    commit

--- src/components/VMobileHeader/index.vue
+++ src/components/VMobileHeader/index.vue
@@ -20,18 +20,21 @@ export default {};
 .mobile-header {
   width: 100%;
   height: 80px;
-  padding-top: 10px;
-  padding-bottom: 10px;
   background-color: $color-white;
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
   z-index: 100;
-  display: flex;
+  display: none;
   align-items: center;
   justify-content: space-between;
-  padding-left: 20px;
-  padding-right: 20px;
+  padding: 10px 20px;
+
+  @media (max-width: 767px) {
+    & {
+      display: flex;
+    }
+  }
 }
 </style>
