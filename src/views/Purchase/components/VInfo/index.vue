7a192931c09851471bc07f0cc50615704570baa6
Halid <denievhalid88@mail.ru>
2022-01-09 06:17:39 +0300

    commit

--- src/views/Purchase/components/VInfo/index.vue
+++ src/views/Purchase/components/VInfo/index.vue
@@ -3,6 +3,74 @@
     <div class="group__title text--blue">
       {{ $t("pages.buying.info") }}
     </div>
+    <div class="list-info__group group">
+      <div class="group__content">
+        <div class="group__item text--bold-700">
+          {{ $t("orderNumber") }}
+        </div>
+        <div class="group__value"></div>
+      </div>
+      <div class="group__content">
+        <div class="group__item text--bold-700">
+          {{ $t("region") }}
+        </div>
+        <div class="group__value"></div>
+      </div>
+      <div class="group__content">
+        <div class="group__item text--bold-700">
+          {{ $t("category") }}
+        </div>
+        <div class="group__value"></div>
+      </div>
+      <div class="group__content">
+        <div class="group__item text--bold-700">
+          {{ $t("author") }}
+        </div>
+        <div class="group__value"></div>
+      </div>
+      <div class="group__content">
+        <div class="group__item text--bold-700">
+          {{ $t("executor") }}
+        </div>
+        <div class="group__value"></div>
+      </div>
+      <div class="group__content">
+        <div class="group__item text--bold-700">
+          {{ $t("description") }}
+        </div>
+        <div class="group__value"></div>
+      </div>
+      <div class="group__content">
+        <div class="group__item text--bold-700">
+          {{ $t("status") }}
+        </div>
+        <div class="group__value"></div>
+      </div>
+      <div class="group__content">
+        <div class="group__item text--bold-700">
+          {{ $t("comment") }}
+        </div>
+        <div class="group__value"></div>
+      </div>
+      <div class="group__content">
+        <div class="group__item text--bold-700">
+          {{ $t("goods") }}
+        </div>
+        <div class="group__value"></div>
+      </div>
+      <div class="group__content">
+        <div class="group__item text--bold-700">
+          {{ $t("dateDelivery") }}
+        </div>
+        <div class="group__value"></div>
+      </div>
+      <div class="group__content">
+        <div class="group__item text--bold-700">
+          {{ $t("created") }}
+        </div>
+        <div class="group__value"></div>
+      </div>
+    </div>
   </div>
 </template>
 
