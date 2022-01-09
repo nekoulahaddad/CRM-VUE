e3a6d78cac689c0b6f502157ae1072d0f9267a93
Halid <denievhalid88@mail.ru>
2022-01-10 01:17:37 +0300

    commit

--- src/components/VSearch/index.vue
+++ src/components/VSearch/index.vue
@@ -8,6 +8,12 @@
       @input="$emit('input', $event.target.value)"
       @keydown.enter.prevent="$emit('submit', $event.target.value)"
     />
+    <img
+      alt=""
+      class="search__icon"
+      src="@/assets/icons/search.svg"
+      @click="$emit('submit')"
+    />
   </div>
 </template>
 
@@ -22,6 +28,8 @@ export default {
 
 <style lang="scss">
 .search {
+  position: relative;
+
   &__input {
     border: 0;
     border-bottom: 1px solid #e6eaf0 !important;
@@ -35,6 +43,13 @@ export default {
     &::placeholder {
       color: rgba(0, 0, 0, 0.3);
     }
+  }
+
+  &__icon {
+    position: absolute;
+    top: 7px;
+    right: 5px;
+    cursor: pointer;
   }
 }
 </style>
