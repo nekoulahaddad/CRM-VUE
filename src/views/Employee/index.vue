f26b5a52c87e3b2a43fb7be7e8bb0bc4e6dbf704
Halid <denievhalid88@mail.ru>
2022-01-10 08:58:29 +0300

    commit

--- src/views/Employee/index.vue
+++ src/views/Employee/index.vue
@@ -247,7 +247,7 @@ export default {
 <style scoped lang="scss">
 .employee-page {
   .list__columns {
-    grid-template-columns: 140px 140px 400px 100px 100px 200px 160px;
+    grid-template-columns: 140px 140px 400px 100px 100px 200px 1fr;
   }
 }
 </style>
