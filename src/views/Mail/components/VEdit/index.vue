3b8333c65907a42cafb7e67f6b01c070be929aaf
Halid <denievhalid88@mail.ru>
2022-01-10 08:53:39 +0300

    commit

--- src/views/Mail/components/VEdit/index.vue
+++ src/views/Mail/components/VEdit/index.vue
@@ -1,4 +1,24 @@
-<template></template>
+<template>
+  <div class="list__info list-info mail-edit-form">
+    <form @submit.prevent="">
+      <div class="edit__inner">
+        <div class="edit__title text--blue">
+          {{ $t("pages.mail.edit") }}
+        </div>
+        <div class="group">
+          <div class="group__title">{{ $t("manager") }}</div>
+          <div class="group__content">
+            <input
+              class="form-control"
+              type="text"
+              placeholder="Введите название задачи..."
+            />
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
 
 <script>
 export default {};
