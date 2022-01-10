0f0c18a8c2a62c03943830d8258297adff1266ad
Halid <denievhalid88@mail.ru>
2022-01-10 07:51:05 +0300

    commit

--- src/components/VButton/index.vue
+++ src/components/VButton/index.vue
@@ -5,7 +5,31 @@
     :class="classes"
     class="btn"
   >
-    <slot />
+    <svg
+      v-if="disabled"
+      xmlns="http://www.w3.org/2000/svg"
+      width="22"
+      height="22"
+      viewBox="0 0 38 38"
+      stroke="#fff"
+    >
+      <g fill="none" fill-rule="evenodd">
+        <g transform="translate(1 1)" stroke-width="2">
+          <circle stroke-opacity=".2" cx="18" cy="18" r="18" />
+          <path d="M36 18c0-9.94-8.06-18-18-18">
+            <animateTransform
+              attributeName="transform"
+              type="rotate"
+              from="0 18 18"
+              to="360 18 18"
+              dur=".4s"
+              repeatCount="indefinite"
+            />
+          </path>
+        </g>
+      </g>
+    </svg>
+    <slot v-else />
   </button>
 </template>
 
