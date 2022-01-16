8cd0f444711bf5d8afaa969eef7be2ac14847681
Halid <denievhalid88@mail.ru>
2022-01-16 13:05:18 +0300

    commit

--- src/views/Monitor/components/VLearn/index.vue
+++ src/views/Monitor/components/VLearn/index.vue
@@ -3,21 +3,10 @@
     <div class="learn__inner">
       <div class="learn__title">Обучение:</div>
       <div class="list">
-        <div class="list__row list__row--shadow list__row--white">
-          <div class="list__columns">
-            <div class="list__column">
-              <div>Обучение для менеджера по онлайн продажам</div>
-            </div>
-          </div>
-        </div>
-        <div class="list__row list__row--shadow list__row--white">
-          <div class="list__columns">
-            <div class="list__column">
-              <div>Обучение для менеджера по онлайн продажам</div>
-            </div>
-          </div>
-        </div>
-        <div class="list__row list__row--shadow list__row--white">
+        <div
+          v-for="i in 3"
+          class="list__row list__row--shadow list__row--white"
+        >
           <div class="list__columns">
             <div class="list__column">
               <div>Обучение для менеджера по онлайн продажам</div>
@@ -56,6 +45,11 @@ export default {};
   .list__row,
   .list__columns {
     height: 40px;
+
+    .list__column {
+      text-align: left;
+      font-weight: 600;
+    }
   }
 }
 </style>
