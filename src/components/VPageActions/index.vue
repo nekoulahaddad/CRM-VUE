73e5dfc90d02bd76df32a0129c9b02b40a4bf9f6
Halid <denievhalid88@mail.ru>
2022-01-17 10:46:06 +0300

    commit

--- src/components/VPageActions/index.vue
+++ src/components/VPageActions/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="page-actions">
     <div class="page-actions__inner">
-      <a href="" class="page-actions__button">
+      <a href="" @click.prevent="clearCache" class="page-actions__button">
         <img src="@/assets/icons/clear_cache.svg" alt="" />
       </a>
       <template v-if="name === 'education'">
@@ -14,10 +14,29 @@
 </template>
 
 <script>
+import axios from "@/api/axios";
+
 export default {
   computed: {
     name() {
       return this.$route.name;
+    },
+  },
+  methods: {
+    clearCache() {
+      axios
+        .get("https://tdcsk.com/api/cache-clear", {
+          headers: {
+            Origin: process.env.VUE_APP_DEVELOP_URL,
+            "Access-Control-Allow-Origin": process.env.VUE_APP_DEVELOP_URL,
+          },
+        })
+        .then(() => {
+          this.$toast.success("Кэш успешно очищен!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
   },
 };
