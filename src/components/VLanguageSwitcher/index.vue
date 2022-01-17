2d0b03282b53a34427299225192ba3b323629834
Halid <denievhalid88@mail.ru>
2022-01-17 09:40:27 +0300

    commit

--- src/components/VLanguageSwitcher/index.vue
+++ src/components/VLanguageSwitcher/index.vue
@@ -1,9 +1,25 @@
 <template>
-  <div class="language-switcher"></div>
+  <div class="language-switcher" @click="toggleSwitcher">
+    <div
+      class="language-switcher__button"
+      :class="{ 'language-switcher__button--toggle': toggle }"
+    />
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      toggle: false,
+    };
+  },
+  methods: {
+    toggleSwitcher() {
+      this.toggle = !this.toggle;
+    },
+  },
+};
 </script>
 
 <style lang="scss">
@@ -11,5 +27,23 @@ export default {};
 
 .language-switcher {
   width: 37px;
+  height: 15.42px;
+  background-color: $color-red;
+  cursor: pointer;
+  position: relative;
+
+  &__button {
+    background-color: $color-white;
+    width: 14.8px;
+    height: 11.68px;
+    top: 2px;
+    left: 2px;
+    transition: left 0.2s ease-in-out;
+    position: absolute;
+
+    &--toggle {
+      left: 19px;
+    }
+  }
 }
 </style>
