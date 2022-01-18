9899683bd57ee3747100c084c484a85cf51934c2
Halid <denievhalid88@mail.ru>
2022-01-18 09:58:54 +0300

    commit

--- src/views/Monitor/components/VClients/index.vue
+++ src/views/Monitor/components/VClients/index.vue
@@ -2,6 +2,50 @@
   <div class="clients">
     <div class="clients__inner">
       <div class="clients__title">Новых клиентов:</div>
+      <div class="client__column column">
+        <div class="column__inner">
+          <div class="column__top">
+            <div class="column__icon"></div>
+            <div class="column__title">Клиенты:</div>
+          </div>
+          <div class="column__bottom">
+            <div class="column__count">2557</div>
+          </div>
+        </div>
+      </div>
+      <div class="client__column column">
+        <div class="column__inner">
+          <div class="column__top">
+            <div class="column__icon"></div>
+            <div class="column__title">Сотрудники:</div>
+          </div>
+          <div class="column__bottom">
+            <div class="column__count">327</div>
+          </div>
+        </div>
+      </div>
+      <div class="client__column column">
+        <div class="column__inner">
+          <div class="column__top">
+            <div class="column__icon"></div>
+            <div class="column__title">Количество дел:</div>
+          </div>
+          <div class="column__bottom">
+            <div class="column__count">540</div>
+          </div>
+        </div>
+      </div>
+      <div class="client__column column">
+        <div class="column__inner">
+          <div class="column__top">
+            <div class="column__icon"></div>
+            <div class="column__title">Загруженность работников:</div>
+          </div>
+          <div class="column__bottom">
+            <div class="column__count">28%</div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
