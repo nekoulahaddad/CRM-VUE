2c1a0da1b2cd0b41c344667606df80eb35631c20
Halid <denievhalid88@mail.ru>
2022-01-18 12:26:12 +0300

    commit

--- src/views/Desktop/index.vue
+++ src/views/Desktop/index.vue
@@ -9,7 +9,13 @@
     <div class="page__body d-flex">
       <div class="page__left">
         <div class="tasks">
-          <div class="tasks__title"></div>
+          <div class="tasks__title">Доска задач</div>
+          <div class="tasks__lists">
+            <div class="tasks__list"></div>
+            <div class="tasks__list"></div>
+            <div class="tasks__list"></div>
+            <div class="tasks__list"></div>
+          </div>
         </div>
       </div>
       <div class="page__right">12</div>
@@ -41,6 +47,28 @@ export default {};
     border-radius: $border-radius;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.006), 0 4px 4px rgba(0, 0, 0, 0.08);
     padding: 10px;
+
+    &__title {
+      font-size: 18px;
+      font-weight: 600;
+      margin-bottom: 10px;
+    }
+
+    &__lists {
+      display: flex;
+    }
+
+    &__list {
+      background-color: $color-gray-secondary;
+      border-radius: $border-radius;
+      padding: 10px;
+      box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+      width: 328px;
+
+      & + * {
+        margin-left: 10px;
+      }
+    }
   }
 }
 </style>
