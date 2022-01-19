032804a34883a82f1606f56c79149a1746f3dd73
Halid <denievhalid88@mail.ru>
2022-01-19 14:24:46 +0300

    commit

--- src/views/Education/components/VDocuments/index.vue
+++ src/views/Education/components/VDocuments/index.vue
@@ -9,10 +9,24 @@
         <div class="list__column">
           <div class="table__actions">
             <div class="table__icon">
-              <img src="@/assets/icons/doc_download_icon.svg" alt="" />
+              <img
+                alt=""
+                @click="
+                  downloadItem(
+                    document.path + document.document,
+                    document.document,
+                    document.title
+                  )
+                "
+                src="@/assets/icons/doc_download_icon.svg"
+              />
             </div>
             <div class="table__icon">
-              <img src="@/assets/icons/trash_icon.svg" alt="" />
+              <img
+                alt=""
+                v-if="role === 'director' && canChanges"
+                src="@/assets/icons/trash_icon.svg"
+              />
             </div>
           </div>
         </div>
@@ -22,11 +36,76 @@
 </template>
 
 <script>
+import axios from "@/api/axios";
+import { mapMutations } from "vuex";
+
 export default {
   props: {
     documents: {
       type: Array,
       required: true,
+    },
+  },
+  computed: {
+    role: {
+      get: function () {
+        return this.getUserRole().role;
+      },
+    },
+    userLastName: {
+      get: function () {
+        return this.getUserRole().surname;
+      },
+    },
+    canChanges: {
+      get: function () {
+        return this.whoCanDoChanges.some(
+          (v) => v === this.userLastName?.trim()
+        );
+      },
+    },
+  },
+  data() {
+    return {
+      typeE: "crm",
+      filtersOptions: {},
+      activeIndex: -1,
+      add: false,
+      edit: false,
+      deleteEducationForm: false,
+      deleteDocument: false,
+      upload: false,
+      editedItem: {},
+      deletedItem: {},
+      educationsArr: [],
+      uploadedItem: {},
+      deletedDocument: {},
+      whoCanDoChanges: ["Якубовский", "Евдокимова", "Аракелов", "Марценюк"],
+    };
+  },
+  methods: {
+    ...mapMutations({
+      changeStatus: "change_load_status",
+    }),
+    downloadItem(url, filename, title) {
+      axios
+        .get(url, { responseType: "blob" })
+        .then((response) => {
+          const link = document.createElement("a");
+          const blob = new Blob([response.data]);
+          let splitedName = filename.split(".");
+          let extension = splitedName[splitedName.length - 1];
+          let urll = window.URL.createObjectURL(blob);
+          link.href = urll;
+          link.download = title + "." + extension;
+          link.click();
+          setTimeout(() => {
+            window.URL.revokeObjectURL(urll);
+            document.body.removeChild(link);
+          }, 0);
+          URL.revokeObjectURL(link.href);
+        })
+        .catch(console.error);
     },
   },
 };
