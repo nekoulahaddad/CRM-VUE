c6759ef77e003768fec104523bc389d5da581f27
Halid <denievhalid88@mail.ru>
2022-01-19 09:51:08 +0300

    commit

--- src/views/Education/components/VCreateSection/index.vue
+++ src/views/Education/components/VCreateSection/index.vue
@@ -1,20 +1,42 @@
 <template>
-  <div class="list">
+  <div class="list create-section-list">
     <div class="list__row list__row--shadow list__row--opened">
       <div
         class="list__columns list__columns--shadow list__columns--white education-list-columns"
       >
         <div class="list__column">Добавить новый раздел:</div>
+        <img
+          @click="$emit('close')"
+          class="close-icon"
+          src="/icons/close_icon.svg"
+          alt=""
+        />
       </div>
 
       <div class="list__info list-info education-list-info">
-        <div class="group__title text--blue">123</div>
-        <div class="list-info__group group">
-          <div class="group__content">
-            <div class="group__item text--bold-700">123</div>
-            <div class="group__value">223</div>
+        <form>
+          <div class="edit__inner">
+            <div class="group">
+              <div class="group__title">Наименование раздела:</div>
+              <div class="group__content">
+                <input
+                  class="form-control"
+                  placeholder="Введите название раздела..."
+                  type="text"
+                />
+              </div>
+            </div>
+            <div class="group">
+              <div class="group__title">Описание раздела::</div>
+              <div class="group__content">
+                <textarea
+                  class="form-textarea"
+                  placeholder="Введите описание раздела..."
+                ></textarea>
+              </div>
+            </div>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   </div>
@@ -23,3 +45,14 @@
 <script>
 export default {};
 </script>
+
+<style lang="scss">
+.create-section-list {
+  .close-icon {
+    cursor: pointer;
+    right: 10px;
+    top: 13px;
+    position: absolute;
+  }
+}
+</style>
