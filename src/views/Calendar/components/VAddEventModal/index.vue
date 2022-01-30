58fb5a4928d1c66ba55d481193021c82e487fb9c
Halid <denievhalid88@mail.ru>
2022-01-30 17:12:20 +0300

    commit

--- src/views/Calendar/components/VAddEventModal/index.vue
+++ src/views/Calendar/components/VAddEventModal/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-modal :adaptive="true" :minHeight="727" :minWidth="1130" name="addEvent">
+  <v-modal :adaptive="true" :minHeight="677" :minWidth="1130" name="addEvent">
     <div class="vm--modal__title">
       Создать мероприятие
       <img
@@ -17,6 +17,46 @@
             type="text"
             class="form-control"
             placeholder="Введите заголовок мероприятия..."
+          />
+        </div>
+      </div>
+    </div>
+    <div class="vm--modal__inner">
+      <div class="group">
+        <div class="group__title">Участники:</div>
+        <div class="group__content">
+          <autocomplete placeholder="Введите исполнителя задачи...">
+            <template #result="{ result, props }">
+              <li v-bind="props">
+                {{ result.surname }}
+              </li>
+            </template>
+          </autocomplete>
+        </div>
+      </div>
+    </div>
+    <div class="vm--modal__inner">
+      <div class="group">
+        <div class="group__title">Дата начала:</div>
+        <div class="group__content">
+          <datetime
+            v-model="start"
+            required
+            input-class="forms__container--input"
+            :phrases="{ ok: $t('ready'), cancel: $t('cancel') }"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="vm--modal__inner">
+      <div class="group">
+        <div class="group__title">Дата окончания:</div>
+        <div class="group__content">
+          <datetime
+            v-model="end"
+            required
+            input-class="forms__container--input"
+            :phrases="{ ok: $t('ready'), cancel: $t('cancel') }"
           />
         </div>
       </div>
@@ -41,5 +81,45 @@ import VButton from "@/components/VButton";
 
 export default {
   components: { VButton },
+  data() {
+    return {};
+  },
+  props: {
+    selectionStart: {
+      type: Date,
+    },
+    selectionEnd: {
+      type: Date,
+    },
+    type: {
+      type: String,
+    },
+  },
+  computed: {
+    start: {
+      get: function () {
+        return this.selectionStart
+          ? new Date(this.$moment(this.selectionStart).format()).toISOString()
+          : new Date(this.$moment()).toISOString();
+      },
+      set: function (date) {
+        let newDate = new Date(this.$moment(date));
+        this.selectionStart = newDate.toISOString();
+        return newDate;
+      },
+    },
+    end: {
+      get: function () {
+        return this.selectionEnd
+          ? new Date(this.$moment(this.selectionEnd).format()).toISOString()
+          : new Date(this.$moment()).toISOString();
+      },
+      set: function (date) {
+        let newDate = new Date(this.$moment(date));
+        this.selectionEnd = newDate.toISOString();
+        return newDate;
+      },
+    },
+  },
 };
 </script>
