69cb11120ac9370237ed8a96a973442d982df844
Halid <denievhalid88@mail.ru>
2022-01-31 17:02:03 +0300

    commit

--- src/views/Sites/index.vue
+++ src/views/Sites/index.vue
@@ -5,13 +5,32 @@
       icon="sites_title"
       :filterToggle="false"
     />
+    <div class="page__body d-flex">
+      <div
+        class="page__right"
+        :class="{
+          'page__right--fluid': sidebar && !showFilter,
+          'page__right--middle': sidebar && showFilter,
+          'page__right--full': !showFilter && !sidebar,
+        }"
+      ></div>
+    </div>
   </div>
 </template>
 
 <script>
 import VPageHeader from "@/components/VPageHeader";
+import { mapGetters } from "vuex";
 
 export default {
   components: { VPageHeader },
+  data() {
+    return {
+      showFilter: false,
+    };
+  },
+  computed: {
+    ...mapGetters({ sidebar: "sidebar" }),
+  },
 };
 </script>
