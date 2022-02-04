b602974d153b35a491c6b74ac2415a35b6499804
Halid <denievhalid88@mail.ru>
2022-02-05 01:44:18 +0300

    commit

--- src/views/Vacancies/components/VInfo/index.vue
+++ src/views/Vacancies/components/VInfo/index.vue
@@ -1,64 +1,62 @@
 <template>
-  <div class="list__info list-info employee-list-info">
+  <div class="list__info list-info infoItem-list-info">
     <!-- Основная информация -->
-    <div class="d-flex justify-content-between">
-      <div class="group__title text--blue">
-        {{ $t("pages.employee.employeeMainInfo") }}
+    <div class="group__title text--blue">
+      {{ $t("pages.employee.employeeMainInfo") }}
+    </div>
+    <div class="list-info__group group">
+      <div class="group__content">
+        <div class="group__item text--bold-700">Должность:</div>
+        <div class="group__value">{{ infoItem.role }}</div>
       </div>
-      <div class="list-info__group group">
-        <div class="group__content">
-          <div class="group__item text--bold-700">Должность:</div>
-          <div class="group__value">{{ infoItem.role }}</div>
+    </div>
+
+    <div class="list-info__group group">
+      <div class="group__content">
+        <div class="group__item text--bold-700">Отдел:</div>
+        <div class="group__value">{{ infoItem.department.title }}</div>
+      </div>
+    </div>
+
+    <div class="list-info__group group">
+      <div class="group__content">
+        <div class="group__item text--bold-700">Регион:</div>
+        <div class="group__value">{{ infoItem.region.title }}</div>
+      </div>
+    </div>
+
+    <div class="list-info__group group">
+      <div class="group__content">
+        <div class="group__item text--bold-700">Требования:</div>
+        <div class="group__value">
+          {{ infoItem.description.requirements }}
         </div>
       </div>
+    </div>
 
-      <div class="list-info__group group">
-        <div class="group__content">
-          <div class="group__item text--bold-700">Отдел:</div>
-          <div class="group__value">{{ infoItem.department.title }}</div>
+    <div class="list-info__group group">
+      <div class="group__content">
+        <div class="group__item text--bold-700">Обязанности:</div>
+        <div class="group__value">
+          {{ infoItem.description.responsibilities }}
         </div>
       </div>
+    </div>
 
-      <div class="list-info__group group">
-        <div class="group__content">
-          <div class="group__item text--bold-700">Регион:</div>
-          <div class="group__value">{{ infoItem.region.title }}</div>
+    <div class="list-info__group group">
+      <div class="group__content">
+        <div class="group__item text--bold-700">Условия:</div>
+        <div class="group__value">
+          {{ infoItem.description.conditions }}
         </div>
       </div>
+    </div>
 
-      <div class="list-info__group group">
-        <div class="group__content">
-          <div class="group__item text--bold-700">Требования:</div>
-          <div class="group__value">
-            {{ infoItem.description.requirements }}
-          </div>
-        </div>
-      </div>
-
-      <div class="list-info__group group">
-        <div class="group__content">
-          <div class="group__item text--bold-700">Обязанности:</div>
-          <div class="group__value">
-            {{ infoItem.description.responsibilities }}
-          </div>
-        </div>
-      </div>
-
-      <div class="list-info__group group">
-        <div class="group__content">
-          <div class="group__item text--bold-700">Условия:</div>
-          <div class="group__value">
-            {{ infoItem.description.conditions }}
-          </div>
-        </div>
-      </div>
-
-      <div class="list-info__group group">
-        <div class="group__content">
-          <div class="group__item text--bold-700">Ссылка:</div>
-          <div class="group__value">
-            {{ infoItem.link }}
-          </div>
+    <div class="list-info__group group">
+      <div class="group__content">
+        <div class="group__item text--bold-700">Ссылка:</div>
+        <div class="group__value">
+          {{ infoItem.link }}
         </div>
       </div>
     </div>
