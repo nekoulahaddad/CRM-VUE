1033ff5232c84120f0bc237571cc254f119efc85
Halid <denievhalid88@mail.ru>
2022-03-01 03:31:02 +0300

    commit

--- src/views/Education/components/VSectionForm/index.vue
+++ src/views/Education/components/VSectionForm/index.vue
@@ -1,70 +1,78 @@
 <template>
-  <form @submit.prevent="$emit('submit', $event)">
-    <div class="edit__inner">
-      <!-- Наименование раздела -->
-      <div class="group">
-        <div class="group__title">Наименование раздела:</div>
-        <div class="group__content">
-          <input
-            required
-            class="form-control"
-            placeholder="Введите название раздела..."
-            type="text"
-            v-model="formData.title"
-            @change="onChange($event)"
-            name="title"
-          />
+  <form class="create-section-form" @submit.prevent="$emit('submit', $event)">
+    <div class="d-flex justify-content-between">
+      <div class="flex-1" style="margin-right: 25px">
+        <!-- Наименование раздела -->
+        <div class="group">
+          <div class="group__title">Наименование раздела:</div>
+          <div class="group__content">
+            <input
+              required
+              class="form-control"
+              placeholder="Введите название раздела..."
+              type="text"
+              v-model="formData.title"
+              @change="onChange($event)"
+              name="title"
+            />
+          </div>
+        </div>
+        <!-- Описание раздела -->
+        <div class="group">
+          <div class="group__title">Описание раздела:</div>
+          <div class="group__content">
+            <textarea
+              required
+              class="form-textarea"
+              placeholder="Введите описание раздела..."
+              name="description"
+              v-model="formData.description"
+              @change="onChange($event)"
+            />
+          </div>
         </div>
       </div>
-      <!-- Для какой роли -->
-      <div class="group">
-        <div class="group__title">Для какой роли:</div>
-        <div class="group__content">
-          <select
-            class="form-select"
-            v-model="formData.role"
-            name="role"
-            @change="onChange($event)"
-          >
-            <option value="all">Все роли</option>
-            <option :value="key" v-for="(role, key) in $t('roles')">
-              {{ role }}
-            </option>
-          </select>
+      <div>
+        <!-- Для какой роли -->
+        <div class="group">
+          <div class="group__title">Для какой роли:</div>
+          <div class="group__content">
+            <select
+              class="form-select"
+              v-model="formData.role"
+              name="role"
+              @change="onChange($event)"
+            >
+              <option value="all">Все роли</option>
+              <option :value="key" v-for="(role, key) in $t('roles')">
+                {{ role }}
+              </option>
+            </select>
+          </div>
+        </div>
+        <!-- Для какого отдела -->
+        <div class="group">
+          <div class="group__title">Для какого отдела:</div>
+          <div class="group__content">
+            <select
+              class="form-select"
+              v-model="formData.department"
+              name="department"
+              @change="onChange($event)"
+            >
+              <option
+                v-for="department in departments"
+                :value="department.value"
+              >
+                {{ department.title }}
+              </option>
+            </select>
+          </div>
         </div>
       </div>
-      <!-- Для какого отдела -->
-      <div class="group">
-        <div class="group__title">Для какого отдела:</div>
-        <div class="group__content">
-          <select
-            class="form-select"
-            v-model="formData.department"
-            name="department"
-            @change="onChange($event)"
-          >
-            <option v-for="department in departments" :value="department.value">
-              {{ department.title }}
-            </option>
-          </select>
-        </div>
-      </div>
-      <!-- Описание раздела -->
-      <div class="group">
-        <div class="group__title">Описание раздела:</div>
-        <div class="group__content">
-          <textarea
-            required
-            class="form-textarea"
-            placeholder="Введите описание раздела..."
-            name="description"
-            v-model="formData.description"
-            @change="onChange($event)"
-          />
-        </div>
-      </div>
-      <v-button red>{{ $t("Отправить") }}</v-button>
     </div>
+
+    <v-button red>{{ $t("Отправить") }}</v-button>
   </form>
 </template>
 
@@ -110,3 +118,11 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.create-section-form {
+  button {
+    margin-top: 10px;
+  }
+}
+</style>
