88a977dc2742146959b53820ecbb66eb7ae14c39
Halid <denievhalid88@mail.ru>
2022-03-03 07:13:05 +0300

    commit

--- src/views/OrgChart/components/VItem/index.vue
+++ src/views/OrgChart/components/VItem/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="department__container">
-    <div :class="classes">{{ item.title }}</div>
+    <div :class="classes">
+      <span class="item__title">{{ item.title }}</span>
+      <div class="table__actions">
+        <div class="table__icon">
+          <img src="@/assets/icons/trash_icon.svg" alt="" />
+        </div>
+      </div>
+    </div>
     <v-item
       :level="level + 1"
       :item="child"
@@ -26,6 +33,7 @@ export default {
   computed: {
     classes() {
       return {
+        item: true,
         department__item: true,
         [`department__item--level-${this.level}`]: true,
       };
@@ -44,6 +52,10 @@ export default {
   align-items: center;
   padding: 10px;
   margin-bottom: 20px;
+  justify-content: space-between;
+  font-size: 14px;
+  font-weight: 700;
+  color: $color-white;
 
   &--level-1 {
     height: 80px;
@@ -52,19 +64,19 @@ export default {
 
   &--level-2 {
     height: 48px;
-    margin-left: 20px;
+    margin-left: 30px;
     background: linear-gradient(90deg, #ff747a, #6f2b8b);
   }
 
   &--level-3 {
     height: 48px;
-    margin-left: 40px;
+    margin-left: 60px;
     background: linear-gradient(90deg, #ff747a, #029faf);
   }
 
   &--level-4 {
     height: 48px;
-    margin-left: 60px;
+    margin-left: 80px;
     background: linear-gradient(90deg, #ff747a, #033e7d);
   }
 }
