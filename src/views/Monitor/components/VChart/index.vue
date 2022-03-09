fbd21a689b2023609d39241792b67725bde9f522
Halid <denievhalid88@mail.ru>
2022-03-09 12:48:34 +0300

    commit

--- src/views/Monitor/components/VChart/index.vue
+++ src/views/Monitor/components/VChart/index.vue
@@ -5,6 +5,7 @@
         Проданное количество товаров за
         {{ months[new Date().getMonth()] }}:
       </div>
+      {{ sales }}
       <VLineChart
         v-if="Object.keys(sales).length"
         :width="0"
