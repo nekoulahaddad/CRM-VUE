b0529982985e97129045cccbbdbad269dd003c04
Halid <denievhalid88@mail.ru>
2022-03-17 15:33:37 +0300

    commit

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,5 @@
 const path = require("path");
-const webpack = require("webpack");
 
 module.exports = {
-  chainWebpack: (config) => {
-    config.plugins.delete("prefetch");
-    config.plugins.delete("preload");
-    config.module.rule("vue").uses.delete("cache-loader");
-    config.module.rule("js").uses.delete("cache-loader");
-    config.module.rule("ts").uses.delete("cache-loader");
-    config.module.rule("tsx").uses.delete("cache-loader");
-    if (process.env.NODE_ENV === "production") {
-      // mutate config for production...
-    } else {
-      // mutate for development...
-    }
-  },
-
-  outputDir: path.resolve(__dirname, "server/build"),
+  outputDir: "/var/www/crm_test_api/server/build",
 };
