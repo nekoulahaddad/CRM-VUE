4efb578339ced301eb3039c5493498d5b1e301d9
TD-CSK <tdcsk20@yandex.ru>
2022-03-17 15:12:41 +0300

    c

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: 'csk_crm',
+      script: 'npm',
+	    args: "run start --prefix /var/www/td-csk-crm-new/server",
+      time: true,
+      exec_mode: 'cluster',
+      instances: 4,
+      autorestart: true,
+      listen_timeout : 5000,
+      max_restarts: 50,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        PORT: 3001
+      }
+    },
+  ],
+}
