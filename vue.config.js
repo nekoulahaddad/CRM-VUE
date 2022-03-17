48bab98511d280c076b1d8195d1294c33bfb739c
Halid <denievhalid88@mail.ru>
2022-03-17 15:44:00 +0300

    commit

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
 const path = require("path");
 module.exports = {
   outputDir: "/var/www/crm_test_api/server/build",
 };
-=======
-const path = require("path");
-
-module.exports = {
-    outputDir: "/var/www/crm_test_api/server/build",
-};
->>>>>>> a8b96fa24880616898c2f25be8bb539bbe433be2
