927aa36b70779d78ca6608c1950c4271f9e3ebb6
Halid <denievhalid88@mail.ru>
2022-03-17 20:22:24 +0300

    commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,20 @@
 module.exports = {
   apps: [
     {
-      name: 'csk_crm',
-      script: 'npm',
-	    args: "run start --prefix /var/www/td-csk-crm-new/server",
+      name: "csk_crm",
+      script: "npm",
+      args: "run start --prefix /var/www/td-csk-crm/server",
       time: true,
-      exec_mode: 'cluster',
+      exec_mode: "cluster",
       instances: 4,
       autorestart: true,
-      listen_timeout : 5000,
+      listen_timeout: 5000,
       max_restarts: 50,
       watch: false,
-      max_memory_restart: '1G',
+      max_memory_restart: "1G",
       env: {
-        PORT: 3001
-      }
+        PORT: 3001,
+      },
     },
   ],
-}
+};
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const path = require("path");
+const webpack = require("webpack");
+
 module.exports = {
-  outputDir: "/var/www/crm_test_api/server/build",
+  chainWebpack: (config) => {
+    config.plugins.delete("prefetch");
+    config.plugins.delete("preload");
+    config.module.rule("vue").uses.delete("cache-loader");
+    config.module.rule("js").uses.delete("cache-loader");
+    config.module.rule("ts").uses.delete("cache-loader");
+    config.module.rule("tsx").uses.delete("cache-loader");
+    if (process.env.NODE_ENV === "production") {
+      // mutate config for production...
+    } else {
+      // mutate for development...
+    }
+  },
+
+  outputDir: path.resolve(__dirname, "server/build"),
 };
