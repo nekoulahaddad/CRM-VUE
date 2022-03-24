04b5815c403b7b6f3d0b04dfeafac0b027db6531
Halid <denievhalid88@mail.ru>
2022-03-24 03:07:10 +0300

    commit

--- server/controllers/excel.js
+++ server/controllers/excel.js
@@ -165,6 +165,7 @@ exports.getExcelFromCategories = async (req, res, next) => {
 
     await generateExcel(req, res, region, categories);
   } catch (error) {
+    return res.status(200).json(error);
     next(error);
   }
 };
