b5de99ee7db1bba7432b21a3ce6a854ca73aa55c
Halid <denievhalid88@mail.ru>
2022-05-19 02:31:09 +0300

    commit

--- server/routes/education.js
+++ server/routes/education.js
@@ -1,23 +1,17 @@
 const express = require("express");
 const router = express.Router();
-import {} from "/";
 
 const EducationController = require("../controllers/education");
 const isAuth = require("../middleware/isAuth");
 
 const uploadDocument = require("../middleware/multer");
-const upload = uploadDocument.single("document");
 
 router.get("/get", isAuth, EducationController.getEducations);
 router.post("/post", isAuth, EducationController.addEducation);
 router.post("/update", isAuth, EducationController.editEducation);
 router.post(
   "/upload",
-  upload(req, res, function (err) {
-    if (err) {
-      return res.status(500).json(err);
-    }
-  }),
+  uploadDocument.single("document"),
   isAuth,
   EducationController.uploadDocument
 );
