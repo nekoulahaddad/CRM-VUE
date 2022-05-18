3ac69ef6a26fa45b14430c19b27a08c707b30c6c
Halid <denievhalid88@mail.ru>
2022-05-19 02:41:15 +0300

    commit

--- server/app.js
+++ server/app.js
@@ -87,7 +87,9 @@ app.use(cors(corsOptionsDeligate));
 app.use(logger.requestLog);
 app.use(limiter);
 app.use(express.json({ limit: "200mb" }));
-app.use(express.urlencoded({ extended: true, limit: "200mb" }));
+app.use(
+  express.urlencoded({ extended: true, limit: "200mb", parameterLimit: 50000 })
+);
 app.disable("view cache");
 app.set("etag", false);
 // app.use(helmet());
