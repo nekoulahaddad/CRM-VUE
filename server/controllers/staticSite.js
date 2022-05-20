44a83cf4daefab55456a07b4ff44131b1a467e73
Dmitry <safronovds92@yandex.ru>
2022-05-20 16:20:01 +0300

    stable

--- server/controllers/staticSite.js
+++ server/controllers/staticSite.js
@@ -1,16 +1,16 @@
-const mongoose = require("mongoose");
-const { exec } = require("child_process");
-const Region = require("../models/regions");
-const Categories = require("../models/categories");
-const Products = require("../models/products");
-const path = require("path");
-const fs = require("fs");
-const Excel = require("exceljs");
-const moment = require("moment");
-const { STATIC_SITES_PATH, TEMP_PATH } = require("../utils/path");
-const { uploadFilesFromTempToFolder } = require("../utils/fs");
-const staticSites = require("../models/staticSites");
-const { EXAMPLES } = require("../utils/excel");
+const mongoose = require('mongoose');
+const { exec } = require('child_process');
+const Region = require('../models/regions');
+const Categories = require('../models/categories');
+const Products = require('../models/products');
+const path = require('path');
+const fs = require('fs');
+const Excel = require('exceljs');
+const moment = require('moment');
+const { STATIC_SITES_PATH, TEMP_PATH } = require('../utils/path');
+const { uploadFilesFromTempToFolder } = require('../utils/fs');
+const staticSites = require('../models/staticSites');
+const { EXAMPLES } = require('../utils/excel');
 
 exports.addManager = async (req, res, next) => {
   try {
@@ -20,7 +20,7 @@ exports.addManager = async (req, res, next) => {
 
     if (!site) {
       return res.status(404).json({
-        message: "Сайт не найден!",
+        message: 'Сайт не найден!',
       });
     }
 
@@ -34,8 +34,7 @@ exports.addManager = async (req, res, next) => {
 };
 
 exports.createSite = async (req, res, next) => {
-  let { folder, url, category_id, categoryName, categories, content, manager } =
-    req.body;
+  let { folder, url, category_id, categoryName, categories, content, manager } = req.body;
   // regions.map( r => mongoose.Types.ObjectId(r))
   // const regionsOfSite = await Region.aggregate()
   //     .match({
@@ -53,8 +52,8 @@ exports.createSite = async (req, res, next) => {
     categories: categories.map((c) => mongoose.Types.ObjectId(c)),
     categoryName: categoryName,
     folder: folder,
-    region: mongoose.Types.ObjectId("5f85ba274a9a5d34e0a45fed"),
-    origin: "н/д",
+    region: mongoose.Types.ObjectId('5f85ba274a9a5d34e0a45fed'),
+    origin: 'н/д',
     content: content,
   };
 
@@ -63,7 +62,7 @@ exports.createSite = async (req, res, next) => {
   }
 
   await staticSites.create(data);
-  res.status(200).send({ message: "ok" });
+  res.status(200).send({ message: 'ok' });
 };
 
 exports.getSites = async (req, res, next) => {
@@ -72,23 +71,23 @@ exports.getSites = async (req, res, next) => {
   let sites = await staticSites
     .aggregate()
     .lookup({
-      from: "users",
-      localField: "manager",
-      foreignField: "_id",
-      as: "manager",
+      from: 'users',
+      localField: 'manager',
+      foreignField: '_id',
+      as: 'manager',
     })
     .project({
-      _id: "$_id",
-      url: "$url",
-      categoryName: "$categoryName",
-      categories: "$categories",
-      updatedAt: "$updatedAt",
-      origin: "$origin",
-      folder: "$folder",
-      uploadedFile: "$uploadedFile",
-      content: "$content",
-      regionId: "$region",
-      manager: "$manager",
+      _id: '$_id',
+      url: '$url',
+      categoryName: '$categoryName',
+      categories: '$categories',
+      updatedAt: '$updatedAt',
+      origin: '$origin',
+      folder: '$folder',
+      uploadedFile: '$uploadedFile',
+      content: '$content',
+      regionId: '$region',
+      manager: '$manager',
     });
   await Promise.all(
     sites.map(async (s) => {
@@ -102,7 +101,7 @@ exports.getSites = async (req, res, next) => {
     })
   );
   if (!sites) {
-    return res.status(404).send({ message: "Нет настроенных сайтов" });
+    return res.status(404).send({ message: 'Нет настроенных сайтов' });
   }
   res.status(200).send({ data: result });
 };
@@ -111,29 +110,29 @@ const getSubCategoriesIds = async (region, categoryIds) => {
   // доработать
   let subCategoriesId = await Categories.aggregate()
     .match({
-      "regions.region": mongoose.Types.ObjectId(region),
-      "regions.category.parent_id": { $in: categoryIds },
+      'regions.region': mongoose.Types.ObjectId(region),
+      'regions.category.parent_id': { $in: categoryIds },
     })
     .project({
       _id: null,
       categories: {
         $filter: {
-          input: "$regions",
-          as: "region",
+          input: '$regions',
+          as: 'region',
           cond: {
-            $eq: ["$$region.region", mongoose.Types.ObjectId(region)],
-            $eq: ["$$region.category.deleted", false],
+            $eq: ['$$region.region', mongoose.Types.ObjectId(region)],
+            $eq: ['$$region.category.deleted', false],
           },
         },
       },
     })
-    .unwind("$categories")
+    .unwind('$categories')
     .project({
-      ids: "$categories.category._id",
+      ids: '$categories.category._id',
     })
     .group({
       _id: null,
-      category_ids: { $addToSet: "$ids" },
+      category_ids: { $addToSet: '$ids' },
     });
   subCategoriesId = subCategoriesId[0]
     ? subCategoriesId[0].category_ids
@@ -146,29 +145,29 @@ const getSubCategoriesIds = async (region, categoryIds) => {
 
 async function getCategoryProducts(categories, region, articles) {
   let myMatch = {
-    "regions.region": mongoose.Types.ObjectId(region),
-    "regions.product.category_id": { $in: categories }, // mongoose.Types.ObjectId(category)
+    'regions.region': mongoose.Types.ObjectId(region),
+    'regions.product.category_id': { $in: categories }, // mongoose.Types.ObjectId(category)
   };
   if (articles) {
-    myMatch["regions.product.article"] = { $in: articles };
+    myMatch['regions.product.article'] = { $in: articles };
   }
   let productsFromCategory = await Products.aggregate()
     .match(myMatch)
     .project({
       product: {
         $filter: {
-          input: "$regions",
-          as: "el",
+          input: '$regions',
+          as: 'el',
           cond: {
             $and: [
               {
-                $eq: ["$$el.region", mongoose.Types.ObjectId(region)],
+                $eq: ['$$el.region', mongoose.Types.ObjectId(region)],
               },
               {
-                $eq: ["$$el.product.deleted", false],
+                $eq: ['$$el.product.deleted', false],
               },
               {
-                $gte: ["$$el.product.cost", 0],
+                $gte: ['$$el.product.cost', 0],
               },
             ],
           },
@@ -176,25 +175,25 @@ async function getCategoryProducts(categories, region, articles) {
       },
     })
     .project({
-      product: "$product.product",
+      product: '$product.product',
     })
-    .unwind("$product")
-    .sort({ "product.article": 1 })
+    .unwind('$product')
+    .sort({ 'product.article': 1 })
     .project({
       _id: 0,
-      article: "$product.article",
-      visible: "$product.visible",
-      id: "$product._id",
-      title: "$product.title",
-      cost: "$product.cost",
-      club_cost: "$product.club_cost",
-      options: "$product.options",
-      images: "$product.images",
-      path: "$product.path",
-      unit: "$product.unit",
-      coef: "$product.coef",
-      description: "$product.description",
-      slug: "$product.slug",
+      article: '$product.article',
+      visible: '$product.visible',
+      id: '$product._id',
+      title: '$product.title',
+      cost: '$product.cost',
+      club_cost: '$product.club_cost',
+      options: '$product.options',
+      images: '$product.images',
+      path: '$product.path',
+      unit: '$product.unit',
+      coef: '$product.coef',
+      description: '$product.description',
+      slug: '$product.slug',
     });
   for (p of productsFromCategory) {
     let options = [];
@@ -235,7 +234,7 @@ exports.updateSite = async (req, res, next) => {
   const { id } = req.body;
   const site = await staticSites.findOne({ _id: mongoose.Types.ObjectId(id) });
   if (!site) {
-    return res.status(404).send({ message: "Сайт неактивен" });
+    return res.status(404).send({ message: 'Сайт неактивен' });
   }
   // if no "category_id" provided, then pick up "categories" array from site
   // ! default region - Moscow
@@ -250,26 +249,27 @@ exports.updateSite = async (req, res, next) => {
   let products = await getCategoryProducts(categoryIds, site.region, null);
 
   writeJson(products, site.folder);
-
-  const command = `npm --prefix /var/www/static_sites/${site.folder} run export`;
-  exec(command, async (error, stdout, stderr) => {
-    if (error !== null) {
-      console.log(`exec error: ${error}`);
-      return res.status(500).send({ message: "Ошибка при обновлении" });
-    }
-    await staticSites.findOneAndUpdate(
-      { _id: mongoose.Types.ObjectId(id) },
-      { origin: "БД" }
-    );
+  if (!site.node) {
+    const command = `npm --prefix /var/www/static_sites/${site.folder} run export`;
+    exec(command, async (error, stdout, stderr) => {
+      if (error !== null) {
+        console.log(`exec error: ${error}`);
+        return res.status(500).send({ message: 'Ошибка при обновлении' });
+      }
+      await staticSites.findOneAndUpdate({ _id: mongoose.Types.ObjectId(id) }, { origin: 'БД' });
+      res.status(200).send({ message: `Сайт ${site.url} обновлен` });
+    });
+  } else {
+    await staticSites.findOneAndUpdate({ _id: mongoose.Types.ObjectId(id) }, { origin: 'БД' });
     res.status(200).send({ message: `Сайт ${site.url} обновлен` });
-  });
+  }
 };
 
 exports.importPriceExcel = async (req, res, next) => {
   const { id } = req.body;
   const site = await staticSites.findOne({ _id: mongoose.Types.ObjectId(id) });
   if (!site) {
-    return res.status(404).send({ message: "Сайт неактивен" });
+    return res.status(404).send({ message: 'Сайт неактивен' });
   }
   const file = req.files.document[0];
   let categoryIds = [];
@@ -287,41 +287,30 @@ exports.importPriceExcel = async (req, res, next) => {
           _id: mongoose.Types.ObjectId(r),
         });
         let _tempWorksheet = await book.getWorksheet(`${_tempRegion.title}`);
-        await _tempWorksheet.eachRow(
-          { includeEmpty: false },
-          (row, rowNumber) => {
-            if (rowNumber != 1) {
-              let product = {};
-              product.article = _tempWorksheet.getCell(`A${rowNumber}`).value;
-              product.title = _tempWorksheet.getCell(`B${rowNumber}`).value;
-              product.cost = _tempWorksheet.getCell(`C${rowNumber}`).value;
-              articles.push(product.article);
-              _tempProductsFromExcel.push(product);
-            }
+        await _tempWorksheet.eachRow({ includeEmpty: false }, (row, rowNumber) => {
+          if (rowNumber != 1) {
+            let product = {};
+            product.article = _tempWorksheet.getCell(`A${rowNumber}`).value;
+            product.title = _tempWorksheet.getCell(`B${rowNumber}`).value;
+            product.cost = _tempWorksheet.getCell(`C${rowNumber}`).value;
+            articles.push(product.article);
+            _tempProductsFromExcel.push(product);
           }
-        );
+        });
         if (!_tempProductsFromExcel.length) {
-          res
-            .status(404)
-            .send({ message: "Импортируемый файл пуст или лист EXCEL пуст!" });
+          res.status(404).send({ message: 'Импортируемый файл пуст или лист EXCEL пуст!' });
         }
         if (site.category_id) {
           categoryIds = await getAllCategoryIds(site.category_id, site.region);
         } else {
           categoryIds = site.categories.map((c) => mongoose.Types.ObjectId(c));
         }
-        let _tempProducts = await getCategoryProducts(
-          categoryIds,
-          mongoose.Types.ObjectId(r),
-          articles
-        );
+        let _tempProducts = await getCategoryProducts(categoryIds, mongoose.Types.ObjectId(r), articles);
         if (!_tempProducts.length) {
-          res.status(404).send({ message: "Несоответствие данных" });
+          res.status(404).send({ message: 'Несоответствие данных' });
         }
         for (p of _tempProducts) {
-          let excelProduct = _tempProductsFromExcel.find(
-            (ep) => ep.article == p.article
-          );
+          let excelProduct = _tempProductsFromExcel.find((ep) => ep.article == p.article);
           if (excelProduct) {
             p.cost = excelProduct.cost;
           }
@@ -330,7 +319,7 @@ exports.importPriceExcel = async (req, res, next) => {
       })
     );
   } else {
-    let worksheet = await book.getWorksheet("Products");
+    let worksheet = await book.getWorksheet('Products');
     await worksheet.eachRow({ includeEmpty: false }, (row, rowNumber) => {
       if (rowNumber != 1) {
         let product = {};
@@ -342,7 +331,7 @@ exports.importPriceExcel = async (req, res, next) => {
       }
     });
     if (!productsFromExcel.length) {
-      res.status(404).send({ message: "Импортируемый файл пуст!" });
+      res.status(404).send({ message: 'Импортируемый файл пуст!' });
     }
     if (site.category_id) {
       categoryIds = await getAllCategoryIds(site.category_id, site.region);
@@ -351,12 +340,10 @@ exports.importPriceExcel = async (req, res, next) => {
     }
     products = await getCategoryProducts(categoryIds, site.region, articles);
     if (!products.length) {
-      res.status(404).send({ message: "Несоответствие данных" });
+      res.status(404).send({ message: 'Несоответствие данных' });
     }
     for (p of products) {
-      let excelProduct = productsFromExcel.find(
-        (ep) => ep.article == p.article
-      );
+      let excelProduct = productsFromExcel.find((ep) => ep.article == p.article);
       if (excelProduct) {
         p.cost = excelProduct.cost;
       }
@@ -364,22 +351,31 @@ exports.importPriceExcel = async (req, res, next) => {
     writeJson(products, site.folder);
   }
 
-  await uploadFilesFromTempToFolder(
-    TEMP_PATH,
-    STATIC_SITES_PATH + "/uploaded",
-    file.filename,
-    ""
-  );
-  const command = `npm --prefix /var/www/static_sites/${site.folder} run export`;
-  exec(command, async (error, stdout, stderr) => {
-    if (error !== null) {
-      console.log(`exec error: ${error}`);
-      return res.status(500).send({ message: "Ошибка при обновлении" });
-    }
-    for (p of products) {
-      let excelProduct = productsFromExcel.find(
-        (ep) => ep.article == p.article
+  await uploadFilesFromTempToFolder(TEMP_PATH, STATIC_SITES_PATH + '/uploaded', file.filename, '');
+  if (!site.node) {
+
+    const command = `npm --prefix /var/www/static_sites/${site.folder} run export`;
+    exec(command, async (error, stdout, stderr) => {
+      if (error !== null) {
+        console.log(`exec error: ${error}`);
+        return res.status(500).send({ message: 'Ошибка при обновлении' });
+      }
+      for (p of products) {
+        let excelProduct = productsFromExcel.find((ep) => ep.article == p.article);
+        if (excelProduct) {
+          p.cost = excelProduct.cost;
+          p.title = excelProduct.title;
+        }
+      }
+      await staticSites.findOneAndUpdate(
+        { _id: mongoose.Types.ObjectId(id) },
+        { origin: 'Excel', uploadedFile: file.filename }
       );
+      res.status(200).send({ message: `Сайт ${site.url} обновлен` });
+    });
+  } else {
+    for (p of products) {
+      let excelProduct = productsFromExcel.find((ep) => ep.article == p.article);
       if (excelProduct) {
         p.cost = excelProduct.cost;
         p.title = excelProduct.title;
@@ -387,10 +383,10 @@ exports.importPriceExcel = async (req, res, next) => {
     }
     await staticSites.findOneAndUpdate(
       { _id: mongoose.Types.ObjectId(id) },
-      { origin: "Excel", uploadedFile: file.filename }
+      { origin: 'Excel', uploadedFile: file.filename }
     );
     res.status(200).send({ message: `Сайт ${site.url} обновлен` });
-  });
+  }
 };
 
 // exports.updateStaticSitesContent = async (req, res, next) => {};
@@ -405,26 +401,19 @@ exports.exportPriceExcel = async (req, res, next) => {
     await Promise.all(
       site.regions.map(async (r) => {
         if (site.category_id) {
-          categoryIds = await getAllCategoryIds(
-            site.category_id,
-            mongoose.Types.ObjectId(r)
-          );
+          categoryIds = await getAllCategoryIds(site.category_id, mongoose.Types.ObjectId(r));
         } else {
           categoryIds = site.categories.map((c) => mongoose.Types.ObjectId(c));
         }
         let _tempRegion = await Region.findOne({
           _id: mongoose.Types.ObjectId(r),
         });
-        let _tempProducts = await getCategoryProducts(
-          categoryIds,
-          mongoose.Types.ObjectId(r),
-          null
-        );
+        let _tempProducts = await getCategoryProducts(categoryIds, mongoose.Types.ObjectId(r), null);
         let _tempWorksheet = workbook.addWorksheet(`${_tempRegion.title}`);
         _tempWorksheet.columns = [
-          { header: "Артикул", key: "article" },
-          { header: "Наименование", key: "title" },
-          { header: "Цена", key: "cost" },
+          { header: 'Артикул', key: 'article' },
+          { header: 'Наименование', key: 'title' },
+          { header: 'Цена', key: 'cost' },
         ];
         _tempWorksheet.columns.forEach((column, i) => {
           if (i === 0) column.width = 15;
@@ -448,9 +437,9 @@ exports.exportPriceExcel = async (req, res, next) => {
     products = await getCategoryProducts(categoryIds, site.region, null);
     let worksheet = workbook.addWorksheet(`Products`);
     worksheet.columns = [
-      { header: "Артикул", key: "article" },
-      { header: "Наименование", key: "title" },
-      { header: "Цена", key: "cost" },
+      { header: 'Артикул', key: 'article' },
+      { header: 'Наименование', key: 'title' },
+      { header: 'Цена', key: 'cost' },
     ];
     worksheet.columns.forEach((column, i) => {
       if (i === 0) column.width = 15;
@@ -465,11 +454,7 @@ exports.exportPriceExcel = async (req, res, next) => {
   }
 
   //!
-  let filePath = path.join(
-    STATIC_SITES_PATH,
-    "templates",
-    `/${site.folder}.xlsx`
-  );
+  let filePath = path.join(STATIC_SITES_PATH, 'templates', `/${site.folder}.xlsx`);
   // let filePath = path.join('./', 'templates', `/${site.folder}.xlsx`);
   await workbook.xlsx.writeFile(filePath);
   res.status(200).download(filePath, `${site.folder}.xlsx`);
